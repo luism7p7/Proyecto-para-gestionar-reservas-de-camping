4774d9dbbc1002d1c20d9c636901b453fc579e61
luism7p7 <luism.munoz@campusucc.edu.co>
2025-10-18 15:36:51 -0500

    adiriendo base del proyecto CampingHub con archivo entidades

---
+++ CampingHub/src/test/java/com/example/CampingHubBase/CampingHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CampingHubBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
