368a9a75c37026152aabc63c9b2af6f332d7b43c
“Natalia” <nathaliafanador13@gmail.com>
2025-10-18 20:05:54 -0500

    Primer commit - Proyecto de gestión de reservas de camping

---
+++ CampingHub/src/main/java/com/repositories/ReservaRepository.java
@@ -0,0 +1,12 @@
+package com.example.CampingHub.repositories;
+
+import com.example.CampingHub.entities.Cliente;
+import com.example.CampingHub.entities.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ReservaRepository extends JpaRepository<Reserva,Long> {
+
+}
