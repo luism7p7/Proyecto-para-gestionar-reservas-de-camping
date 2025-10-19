e4da1073f6ca9d1921fe1c9dc3056ba2e2b1a437
Luism7p7 <luism.munoz@campusucc.edu.co>
2025-10-19 18:22:10 -0500

    agregandobase del proyecto con el package entities

---
+++ Base_CampingHub/src/main/java/com/example/Base_CampingHub/BaseCampingHubApplication.java
@@ -0,0 +1,40 @@
+package com.example.Base_CampingHub;
+
+import com.example.Base_CampingHub.entities.Cliente;
+import com.example.Base_CampingHub.entities.Finca;
+import com.example.Base_CampingHub.repositories.ClienteRepository;
+import com.example.Base_CampingHub.repositories.FincaRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CampingHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampingHubApplication.class, args);
+    }
+
+
+    @Bean
+    public CommandLineRunner loadData(FincaRepository fincaRepository, ClienteRepository clienteRepository) {
+        return args -> {
+            System.out.println("Cargando datos de prueba ...");
+
+            Finca fincaDePrueba = new Finca();
+            fincaDePrueba.setNombre("finca el descanso");
+            fincaDePrueba.setUbicacion("Guatape, Antioquia");
+            fincaDePrueba.setCapacidad(10);
+            fincaRepository.save(fincaDePrueba);
+
+            Cliente clienteDePrueba = new Cliente();
+            clienteDePrueba.setNombre("Luis Miguel");
+            clienteDePrueba.setCorreo("Lmunosrendon123@gmail.com");
+            clienteDePrueba.setContraseña("12345678");
+            clienteRepository.save(clienteDePrueba);
+
+            System.out.println("datos de prueba cargados: 1 finca y un cliente con ID 1.");
+        };
+    }
+}
---
+++ Base_CampingHub/src/test/java/com/example/Base_CampingHub/BaseCampingHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Base_CampingHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseCampingHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
